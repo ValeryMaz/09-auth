bcba0a6c592c88026e541df0f66ce552fd56fc9c
ValeryMaz <mazhytova1996@gmail.com>
2025-08-29 18:57:28 -0400

    f

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -3,7 +3,7 @@ import {
   dehydrate,
   HydrationBoundary,
 } from "@tanstack/react-query";
-import { fetchNoteById } from "../../../../lib/api/clientApi";
+import { fetchNoteById } from "../../../../lib/api/serverApi";
 import NoteDetailsClient from "./NoteDetails.client";
 import { Metadata } from "next";
 
